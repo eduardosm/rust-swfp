ce9880650df13bd1ef3e4c1fd3f97d9684b21139
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2026-09-19 23:33:25 +0200

    ci: fail if generated constants change

--- ci/check-generator.sh
+++ ci/check-generator.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 . ci/utils.sh
 
 begin_group "Install Julia packages"
-julia -e 'import Pkg; Pkg.add("Remez"); Pkg.add("SpecialFunctions")'
+julia -e 'import Pkg; Pkg.add(name="Remez", version="0.1.1"); Pkg.add("SpecialFunctions")'
 end_group
 
 begin_group "Fetch dependencies"
@@ -20,5 +20,5 @@ cargo build --frozen -p swfp
 end_group
 
 begin_group "Show diff"
-git diff
+git diff --exit-code
 end_group
